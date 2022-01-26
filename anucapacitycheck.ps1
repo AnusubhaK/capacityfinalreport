4afac7b6fdc80cd59fed609d66da39cd0d5c2106
AnusubhaK <90177553+AnusubhaK@users.noreply.github.com>
2022-01-26 17:45:06 +0000

    Update anucapacitycheck.ps1

--- anucapacitycheck.ps1
+++ anucapacitycheck.ps1
@@ -1,12 +1,14 @@
-Get-Cluster -Name SA-Compute-01 |
+$ClusterName = $args[0]
+Get-Cluster -Name $ClusterName |
 Get-VMHost|
 Select Name,
-@{N='CPU GHz Capacity';E={[math]::Round($_.CpuTotalMhz/1000,2)}},
-@{N='CPU Core';E={[math]::$_.NumCpu}},
-@{N='CPU GHz Core';E={[math]::Round($_.CpuTotalMhz/(1000*$_.NumCpu),2)}},
-@{N='CPU GHz Used';E={[math]::Round($_.CpuUsageMhz/1000,2)}},
-@{N='CPU GHz Free';E={[math]::Round(($_.CpuTotalMhz - $_.CpuUsageMhz)/1000,2)}},
-@{N='Memory Capacity GB';E={[math]::Round($_.MemoryTotalGB,2)}},
-@{N='Memory Used GB';E={[math]::Round($_.MemoryUsageGB,2)}},
-@{N='Memory Free GB';E={[math]::Round(($_.MemoryTotalGB - $_.MemoryUsageGB),2)}} |
+@{N='Total CPU(GHz)';E={[math]::Round($_.CpuTotalMhz/1000,2)}},
+@{N='Total CPU(/Core)';E={$_.NumCpu}},
+#@{N='Clock Speed';E={[math]::Round($_.CpuTotalMhz/(1000*$_.NumCpu),2)}},
+@{N='Used CPU(GHz)';E={[math]::Round($_.CpuUsageMhz/1000,2)}},
+@{N='Free CPU(GHz)';E={[math]::Round(($_.CpuTotalMhz - $_.CpuUsageMhz)/1000,2)}},
+@{N='Free CPU(/Core)';E={[math]::Round(((($_.CpuTotalMhz - $_.CpuUsageMhz)/1000)/($_.CpuTotalMhz/(1000*$_.NumCpu))),0)}},
+@{N='Total Memory(GB)';E={[math]::Round($_.MemoryTotalGB,2)}},
+#@{N='Memory Used GB';E={[math]::Round($_.MemoryUsageGB,2)}},
+@{N='Free Memory(GB)';E={[math]::Round(($_.MemoryTotalGB - $_.MemoryUsageGB),2)}} |
 Export-Csv -Path C:\report.csv -NoTypeInformation -UseCulture
