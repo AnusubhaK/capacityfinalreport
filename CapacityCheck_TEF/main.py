95b8d9762dcb715c62d5068a37bbd2c48f2cd6c6
Surya Prakash Ganesan (MS/ECB32-PS) <SuryaPrakash.Ganesan@in.bosch.com>
2022-02-06 21:25:57 +0530

    completed calculations for worker and master nodes
    - tested with various inputs

--- CapacityCheck_TEF/main.py
+++ CapacityCheck_TEF/main.py
@@ -11,18 +11,61 @@ import json
 InputWorkerNodePool = []
 InputBusiness = []
 
-CalcPodLoadDict = {
-                "CPU": 0,
-                "RAM": 0,
-                "Storage": 0,
-}
+# Constant settings
+MasterNodeCapacity = [
+            {   
+                "Capacity" : 10, # Capacity for <= 10
+                "ReqMasterNodeCnt" : 3, # Required master node count
+                "CPU": 2,
+                "RAM": 8,
+                "Storage": 50
+            },
+            {   
+                "Capacity" : 100, # Capacity for <= 100
+                "ReqMasterNodeCnt" : 5, # Required master node count
+                "CPU": 4,
+                "RAM": 16,
+                "Storage": 50
+            },
+            {  
+                "Capacity" : 250, # Capacity for <= 250
+                "ReqMasterNodeCnt" : 7, # Required master node count
+                "CPU": 8,
+                "RAM": 32,
+                "Storage": 50
+            },
+            {   
+                "Capacity" : 500, # Capacity for <= 500
+                "ReqMasterNodeCnt" : 10, # Required master node count
+                "CPU": 16,
+                "RAM": 64,
+                "Storage": 50
+            }            
+]
 
-CalcReqWorkersDict = {
-                "CPU": 0,
-                "RAM": 0,
-                "Storage": 0,
-                "Count": 0
-}
+#Result variables defintion
+ResultCalculations_WN = [
+    {   # Capacity Worker node pool calculated from web input json
+        #"Web_NoOfWorkerNodePool" : 0, 
+        "Web_TotalNoOfWorkerNodes" : 0, 
+        "CPU": 0,
+        "RAM": 0,
+        "Storage": 0,
+    },
+    {   # Capacity master nodes calculated from predefined constant
+        "Const_NoOfMasterNodes" : 0, 
+        "CPU": 0,
+        "RAM": 0,
+        "Storage": 0,
+    },
+    {   # Capacity Net total worker nodes including worker node (WorkerNodePool + WorkerNode)
+        "Net_NoOfNodes" : 0, 
+        "CPU": 0,
+        "RAM": 0,
+        "Storage": 0,
+    }    
+]
+
 
 # JSON parser to read WorkerNodePool details
 def readjsonwebform(filpath):
@@ -32,6 +75,32 @@ def readjsonwebform(filpath):
     for WorkerNode in data['Capacity']['WorkerNodePool']:
         InputWorkerNodePool.append (WorkerNode)
     filejson.close()
+
+# Calculate WorkerNode
+def calculate_workernode():
+    
+    # Calculate required worker nodes from worker node pool
+    for WorkerNode in InputWorkerNodePool:
+        #ResultCalculations_WN[0]['Web_NoOfWorkerNodePool'] = ResultCalculations_WN[0]['Web_NoOfWorkerNodePool'] + 1
+        ResultCalculations_WN[0]['Web_TotalNoOfWorkerNodes'] = ResultCalculations_WN[0]['Web_TotalNoOfWorkerNodes'] + WorkerNode['Nodecount']
+        ResultCalculations_WN[0]['CPU'] = ResultCalculations_WN[0]['CPU'] + (WorkerNode['Nodecount'] * WorkerNode['NodeCPU'])
+        ResultCalculations_WN[0]['RAM'] = ResultCalculations_WN[0]['RAM'] + (WorkerNode['Nodecount'] * WorkerNode['NodeRAM'])
+        ResultCalculations_WN[0]['Storage'] = ResultCalculations_WN[0]['Storage'] + (WorkerNode['Nodecount'] * WorkerNode['NodeStorage'])
+    
+    # Calculate required Master Node from required workernodes
+    for MasterNodeConst in MasterNodeCapacity:
+        if (ResultCalculations_WN[0]['Web_TotalNoOfWorkerNodes'] <= MasterNodeConst['Capacity']):
+            ResultCalculations_WN[1]['Const_NoOfMasterNodes'] = MasterNodeConst['ReqMasterNodeCnt']
+            ResultCalculations_WN[1]['CPU'] = MasterNodeConst['ReqMasterNodeCnt'] * MasterNodeConst['CPU']
+            ResultCalculations_WN[1]['RAM'] = MasterNodeConst['ReqMasterNodeCnt'] * MasterNodeConst['RAM']
+            ResultCalculations_WN[1]['Storage'] = MasterNodeConst['ReqMasterNodeCnt'] * MasterNodeConst['Storage']
+            break
+    
+    # Calculate consolidated net capacity requirement
+    ResultCalculations_WN[2]['Net_NoOfNodes'] = ResultCalculations_WN[0]['Web_TotalNoOfWorkerNodes'] + ResultCalculations_WN[1]['Const_NoOfMasterNodes']
+    ResultCalculations_WN[2]['CPU'] = ResultCalculations_WN[0]['CPU'] + ResultCalculations_WN[1]['CPU']
+    ResultCalculations_WN[2]['RAM'] = ResultCalculations_WN[0]['RAM'] + ResultCalculations_WN[1]['RAM']
+    ResultCalculations_WN[2]['Storage'] = ResultCalculations_WN[0]['Storage'] + ResultCalculations_WN[1]['Storage']
 
 
 
@@ -47,12 +116,15 @@ if __name__ == "__main__":
 
     #Read input JSON files
     readjsonwebform(args.webjson)
-    print (InputWorkerNodePool)
-    print (InputBusiness)
 
     ResultFile = open(args.outfile, "w") 
     
     #Calcualtions
+    calculate_workernode()
+    print ( ResultCalculations_WN[0])
+    print ( ResultCalculations_WN[1])
+    print (ResultCalculations_WN[2])
+    
 
     ResultFile.write ("-----------------------------------------------------------------\n")       
     ResultFile.write ("               Worker Node Calculation Script                    \n")
